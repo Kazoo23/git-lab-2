080be3647223b4e65771bf64a4a6f3c1ed395c9d
Jackson <pealerjack@gmail.com>
2022-09-06 17:23:52 -0400

    adding lab programm

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Jackson Pealer>
+ *        Date: <9/6/2022>
+ * Description: git test program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
